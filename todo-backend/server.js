f3ee5b1545d60f7e6049c9dc6479df30afb5ee08
Sairam <sairamramavath01@gmail.com>
2024-02-16 14:20:05 +0530

    Added cors

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -8,12 +8,7 @@ const userRouter = require("./routes/userRoute");
 const todoRouter = require("./routes/todoRoute");
 
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
-const corsOptions = {
-  origin: "https://beamish-lollipop-19b546.netlify.app/",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(cors());
 app.use(express.json());
 connectDB();
